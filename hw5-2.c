ec97acd53fc65e03f7497548a7a56ce101b58168
roof311 <146065407+roof311@users.noreply.github.com>
2023-11-07 01:38:29 +0800

    Update hw5-2.c

--- hw5-2.c
+++ hw5-2.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+#include <math.h>
 
+int main() {
+    int minutes;
+    double cost;
+
+    printf("請輸入通話分鐘數：");
+    scanf("%d", &minutes);
+
+    if (minutes <= 800) {
+        cost = minutes * 0.9;
+    } else if (minutes > 800 && minutes <= 1500) {
+        cost = 800 * 0.9 + (minutes - 800) * 0.9 * 0.9;
+    } else {
+        cost = 800 * 0.9 + 700 * 0.9 * 0.9 + (minutes - 1500) * 0.9 * 0.79;
+    }
+
+    cost = round(cost * 10) / 10;
+
+    printf("通話費用為：%.1f\n", cost);
+
+    return 0;
+}
