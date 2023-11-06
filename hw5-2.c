52ccf4234cd5973372bec2e37f1d91f143cf443a
roof311 <146065407+roof311@users.noreply.github.com>
2023-11-07 02:50:12 +0800

    Update hw5-2.c

--- hw5-2.c
+++ hw5-2.c
@@ -10,7 +10,7 @@ int main() {
 
     if (minutes <= 800) {
         cost = minutes * 0.9;
-    } else if (minutes > 800 && minutes <= 1500) {
+    } else if (minutes > 800 && minutes < 1500) {
         cost = 800 * 0.9 + (minutes - 800) * 0.9 * 0.9;
     } else {
         cost = 800 * 0.9 + 700 * 0.9 * 0.9 + (minutes - 1500) * 0.9 * 0.79;
